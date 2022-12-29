94b0a28a8321de8d509950b8abbbbbb822565e8c
adarshdhakad <adarsh@getswipe.in>
2022-12-29 11:45:54 +0530

    Initial commit

---
+++ tableview/src/main/java/com/swipe/tableview/TableView.kt
@@ -0,0 +1,11 @@
+package com.swipe.tableview
+
+import android.content.Context
+import android.widget.Toast
+
+class TableView {
+    fun show(context: Context){
+        Toast.makeText(context , "hii" , Toast.LENGTH_SHORT).show()
+    }
+
+}
